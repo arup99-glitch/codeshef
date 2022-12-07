4a84bf40fcee136021d9da6f01fcfff3b8be38bb
Arup Chakraborty <86925893+arup99-glitch@users.noreply.github.com>
2022-12-08 00:31:44 +0600

    Update Make AP.cpp

--- Make AP.cpp
+++ Make AP.cpp
@@ -1,1 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int m,a,b;
+    cin>>m;
+    for(int i=0;i<m;i++)
+    {
+        cin>>a>>b;
+        if((a+b)%2==0)cout<<((a+b)/2)<<endl;
+        else cout<<-1<<endl;
+    }
+}
