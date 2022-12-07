b384583979150e8a7f3e9c4372ba6115437355ca
Arup Chakraborty <86925893+arup99-glitch@users.noreply.github.com>
2022-12-08 00:30:23 +0600

    Update Boring String.cpp

--- Boring String.cpp
+++ Boring String.cpp
@@ -1,1 +1,37 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,t;
+     string s;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+       cin>>t;
+
+       cin>>s;
+
+    string c=s;
+    int ct=0;
+    sort(s.begin(),s.end());
+    if(c==s){
+    for(int i=0;i<t;i++)
+    {
+        if(s[i]==s[i+1])ct++;
+
+    }
+    cout<<ct<<endl;
+    }
+    else{
+            for(int i=0;i<t;i++)
+    {
+        if(s[i]==s[i+1])ct++;
+
+    }
+    cout<<ct-1<<endl;
+    }
+    }
+
+
+}
