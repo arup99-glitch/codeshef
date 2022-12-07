4c23a5c48d1f620f6569bdd1654233469d3d6d90
Arup Chakraborty <86925893+arup99-glitch@users.noreply.github.com>
2022-12-08 00:31:02 +0600

    Update Expense LIst.cpp

--- Expense LIst.cpp
+++ Expense LIst.cpp
@@ -1,1 +1,19 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,a,b;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a>>b;
+        int x;
+        x=pow(2,b);
+        while(a--)
+        {
+            x=x/2;
+        }
+        cout<<x<<endl;
+    }
+}
