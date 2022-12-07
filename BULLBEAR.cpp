d95464205c0de7cfc121a20e129ee57eefba8f32
Arup Chakraborty <86925893+arup99-glitch@users.noreply.github.com>
2022-12-08 00:28:44 +0600

    Update BULLBEAR.cpp

--- BULLBEAR.cpp
+++ BULLBEAR.cpp
@@ -1,1 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,a,b;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a>>b;
+        if(a>b)cout<<"LOSS"<<endl;
+        if(a<b)cout<<"PROFIT"<<endl;
+        else if(a==b)cout<<"NEUTRAL"<<endl;
+    }
+}
 
